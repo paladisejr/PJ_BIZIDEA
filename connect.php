d3e66909c05ba92214320ce28b6fab128c2584c5
phakphoom chaikla <ohakimaruo@hotmail.com>
2013-06-06 11:11:37 +0700

    connet

--- connect.php
+++ connect.php
@@ -1,13 +1,17 @@
 <?php
-$host="localhost";
-$user="root";
-$pass="password";
-$dbname="test";
-$connect=mysql_connect($host,$user,$pass);
-mysql_select_db($dbname) ;
-mysql_query("SET NAMES utf8",$connect);
-if(!$connect) {
-echo "ไม่สามารถเชื่อมต่อฐานข้อมูลได้".mysql_error();
-die();
-}
+	session_start();
+	mysql_connect("localhost","root","password");
+	mysql_select_db("test");
+    mysql_query("SET NAMES UTF8");
+    if( ! empty($_SESSION['UserID']) )
+    {
+    	$user_id = $_SESSION['UserID'];
+    }
+    else
+    {
+    	$user_id = '';
+    }
+	$strSQL = "SELECT * FROM  `member` WHERE UserID = '".$user_id."' ";
+	$objQuery = mysql_query($strSQL);
+	$objResult = mysql_fetch_array($objQuery);
 ?>
